f1a6ce28eed8161938145fd87da9dd7d7147d5e5
wqhot <wqhot@outlook.com>
2021-05-26 01:57:02 +0800

    fix: 直接转发

--- src/main.cpp
+++ src/main.cpp
@@ -72,57 +72,38 @@ bool recv(uint8_t *buffer)
 
 bool recv(float &o2)
 {
-    const size_t len = 4;
+    const size_t len = 10;
     uint8_t buffer[len] = {0};
     serial_o2.write("read O2 Data");
-    int c = serial_o2.read();
-    while (c >= 0)
+    size_t l = serial_o2.readBytes(buffer, len);
+    if (l != len)
     {
-        if (c=='=')
-        {
-            break;
-        }
-        else
-        {
-            c = serial_o2.read();
-        }
-    }
-    int length = 0;
-    c = serial_o2.read();
-    while (c > 0 && (length < len))
-    {
-        buffer[length] = c;
-        ++length;
-        c = serial_o2.read();
-    }
-    if (length != len)
-    {
-        o2 = -1;
         return false;
     }
-    o2 = 10.0f * (buffer[0] - '0') + 1.0f * (buffer[1] - '0') + 0.1f * (buffer[3] - '0');
+    o2 = 10.0f * (buffer[3] - '0') + 1.0f * (buffer[4] - '0') + 0.1f * (buffer[6] - '0');
     return true;
 }
 
 void loop()
 {
     uint8_t buffer[23];
+    uint8_t bufferNew[10];
     uint8_t sum;
     float o2;
     digitalWrite(LED_PIN, LOW); //小灯亮
     delay(500);
-    if (!recv(buffer + 12))
+    if (!recv(bufferNew))
     {
         serial_report.write("No sensor\n");
     }
     else
     {
-        getId(buffer);
-        // setNum(buffer + 12, o2);
-        sum = getSum(buffer, 22);
-        buffer[22] = sum;
-        serial_report.write(head, 4);
-        serial_report.write(buffer, 23);
+        // getId(buffer);
+        // // setNum(buffer + 12, o2);
+        // sum = getSum(buffer, 22);
+        // buffer[22] = sum;
+        serial_report.write(bufferNew, 10);
+        // serial_report.write(buffer, 23);
     }
     digitalWrite(LED_PIN, HIGH); //小灯灭
     delay(1000);
